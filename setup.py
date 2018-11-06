a04693e6d93e4d599c8db0cd9be09b575d969df7
Santiago <santiagoropb@gmail.com>
2018-11-06 12:54:13 -0500

    Changes setup to automatically find ipopt configuration

--- setup.py
+++ setup.py
@@ -1,22 +1,36 @@
 # Originally contributed by Lorne McIntosh.
 # Modified by Eric Xu
-# Further modification by random internet people.
+# Modified by Jose Santiago Rodriguez
 
-# You will probably have to edit this file in unpredictable ways
-# if you want pyipopt to work for you, sorry.
-
-# When I installed Ipopt from source, I used the
-# --prefix=/usr/local
-# option, so this is where I want pyipopt to look for my ipopt installation.
-# I only installed from source because the ipopt packaging
-# for my linux distribution was buggy,
-# so by the time you read this the bugs have probably been fixed
-# and you will want to specify a different directory here.
-IPOPT_DIR = '/usr/local/'
-
+import subprocess
 import os
 from distutils.core import setup
+from distutils.spawn import find_executable
 from distutils.extension import Extension
+
+def get_ipopt_executable():
+
+    # Find ipopt executable
+    ipopt_executable = None
+
+    for executable in ["ipopt"]:
+        ipopt_executable = find_executable(executable)
+        if ipopt_executable is not None:
+            # Check that SWIG version is ok
+            output = subprocess.check_output([ipopt_executable, "-v"]).decode('utf-8')
+            ipopt_version = output.replace("Ipopt", "")
+            ipopt_version = ipopt_version.replace("\n", "")
+
+    if ipopt_executable is None:
+        raise OSError("Unable to find IPOPT.")
+    print("Found IPOPT: %s (version %s)" % (ipopt_executable, ipopt_version))
+    return ipopt_executable
+
+ipopt_executable = get_ipopt_executable()
+ipopt_bin = os.path.dirname(ipopt_executable)
+ipopt_base = os.path.dirname(ipopt_bin)
+IPOPT_DIR = ipopt_base
+
 
 # NumPy is much easier to install than pyipopt,
 # and is a pyipopt dependency, so require it here.
@@ -35,7 +49,11 @@ IPOPT_LIB = get_ipopt_lib()
 if IPOPT_LIB is None:
     raise Exception('failed to find ipopt lib')
 
-IPOPT_INC = os.path.join(IPOPT_DIR, 'include/coin/')
+ipopt_include = os.path.join(ipopt_base, 'include', 'coin')
+IPOPT_INC = ipopt_include
+
+print("\tInclude directory: {}".format(ipopt_include))
+print("\tLibraries directory: {}".format(IPOPT_LIB))
 
 FILES = ['src/callback.c', 'src/pyipoptcoremodule.c']
 
